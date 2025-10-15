1cb08ce6b4c60d3cdc9d3851017846712510b853
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-10-15 21:48:58 +0200

    Player: add dashing

--- UnityGameKiara/Assets/Scripts/PlayerScript.cs
+++ UnityGameKiara/Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,10 @@ using UnityEngine.InputSystem;
 public class PlayerScript : MonoBehaviour
 {
     //TODO: Coyote time?
-    //TODO: Dash ability
+    //TODO: Dash bounces
+
+    private bool _dashing = false;
+    private int _lastMovementDirection = 0;
 
     private Rigidbody2D _rigidBody;
     private BoxCollider2D _boxCollider;
@@ -17,12 +20,23 @@ public class PlayerScript : MonoBehaviour
     private InputActionReference _movementAction;
     [SerializeField]
     private InputActionReference _jumpAction;
+    [SerializeField]
+    private InputActionReference _dashAction;
 
     [Header("Player stats")]
     [SerializeField]
     private float _movementSpeed = 15.0f;
     [SerializeField]
     private float _jumpPower = 24.0f;
+    [SerializeField]
+    private float _dashSpeed = 64.0f;
+
+    public bool Dashing { get => _dashing; }
+
+    public float MovementSpeed { get => _movementSpeed; set => _movementSpeed = value; }
+    public float JumpPower { get => _jumpPower; set => _jumpPower = value; }
+    public float DashSpeed { get => _dashSpeed; set => _dashSpeed = value; }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
@@ -32,12 +46,21 @@ public class PlayerScript : MonoBehaviour
         _boxCollider = GetComponent<BoxCollider2D>();
 
         _jumpAction.action.started += OnJumpActionStarted;
+        _dashAction.action.started += OnDashActionStarted;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        _rigidBody.linearVelocityX = _movementAction.action.ReadValue<float>() * _movementSpeed;
+        float movementDirection = _movementAction.action.ReadValue<float>();
+
+        if (!_dashing)
+            _rigidBody.linearVelocityX = movementDirection * _movementSpeed;
+
+        if (movementDirection > 0.0f)
+            _lastMovementDirection = 1;
+        else if (movementDirection < 0.0f)
+            _lastMovementDirection = -1;
     }
 
     // Are we on the ground?
@@ -60,9 +83,41 @@ public class PlayerScript : MonoBehaviour
         _rigidBody.linearVelocityY = _jumpPower;
     }
 
+    // Dash in last moved direction
+    private void Dash()
+    {
+        _dashing = true;
+
+        if (_lastMovementDirection == 0)
+            _lastMovementDirection = 1;
+
+        //down-left or down-right depending on last direction moved
+        float dashAngle = (_lastMovementDirection == 1) ? -Mathf.PI / 4.0f : -Mathf.PI / 4.0f * 3.0f;
+
+        Vector2 dashDirection = new(Mathf.Cos(dashAngle), Mathf.Sin(dashAngle));
+
+        _rigidBody.linearVelocity = dashDirection * _dashSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //TODO: ground splat effect
+        if (_dashing)
+        {
+            _dashing = false;
+            Debug.Log("Player Splat!");
+        }
+    }
+
     private void OnJumpActionStarted(InputAction.CallbackContext obj)
     {
         if (IsGrounded())
             Jump();
+    }
+
+    private void OnDashActionStarted(InputAction.CallbackContext obj)
+    {
+        if (!IsGrounded() && !_dashing)
+            Dash();
     }
 }
