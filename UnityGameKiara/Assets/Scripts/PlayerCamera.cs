a301f0e50b136f585ba4bbf5a80d0feb75dc4acb
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-10-18 20:47:23 +0200

    Camera: smooth damp towards player's velocity

--- UnityGameKiara/Assets/Scripts/PlayerCamera.cs
+++ UnityGameKiara/Assets/Scripts/PlayerCamera.cs
@@ -1,29 +1,51 @@
+using DG.Tweening;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class PlayerCamera : MonoBehaviour
 {
-    //TODO: option to offset camera in player's movement direction
     //TODO: tweening for switching between camera modes & changing offset
+    //TODO: camera bounds
     //TODO: mode for fitting a rectangle on screen (least important, could just design around this)
 
-    private Camera _camera;
+    [SerializeField]
+    private Rigidbody2D _follow;
 
     [SerializeField]
-    private Transform _follow;
+    private Vector3 _offset = Vector3.zero;
+    private Vector3 _offsetVelocity = Vector3.zero;
 
-    // Transform to follow.
-    public Transform Follow { get => _follow; set => _follow = value; }
+    // Smoothing times per axis for offset smooth damping.
+    [SerializeField]
+    private Vector3 _smoothTimes = new(0.3f, 2.0f, 0.0f);
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Rigid body to follow.
+    public Rigidbody2D Follow { get => _follow; set => _follow = value; }
+
+    
+
+    private Vector3 SmoothDampVector3(Vector3 current, Vector3 target, ref Vector3 velocity, Vector3 smoothTimes)
     {
-        _camera = GetComponent<Camera>();
+        return new(
+            Mathf.SmoothDamp(current.x, target.x, ref velocity.x, smoothTimes.x),
+            Mathf.SmoothDamp(current.y, target.y, ref velocity.y, smoothTimes.y),
+            Mathf.SmoothDamp(current.z, target.z, ref velocity.z, smoothTimes.z)
+            );
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.position = new Vector3(_follow.position.x, _follow.position.y, transform.position.z);
+        Vector3 targetOffset = new(0.0f, 0.0f, -10.0f);
+
+        if (_follow.linearVelocityX != 0.0f)
+            targetOffset.x = Mathf.Sign(_follow.linearVelocityX) * 6.0f;
+
+        if (_follow.linearVelocityY != 0.0f)
+            targetOffset.y = Mathf.Sign(_follow.linearVelocityY) * 3.0f;
+
+        _offset = SmoothDampVector3(_offset, targetOffset, ref _offsetVelocity, _smoothTimes);
+
+        transform.position = new Vector3(_follow.position.x, _follow.position.y) + _offset;
     }
 }
