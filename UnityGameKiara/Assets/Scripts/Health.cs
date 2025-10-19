645e9c1be31ef3dcde80b8634e9c29291f41aa59
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-10-19 18:39:18 +0200

    Health: add unity events for taking damage & deaths

--- UnityGameKiara/Assets/Scripts/Health.cs
+++ UnityGameKiara/Assets/Scripts/Health.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
@@ -9,16 +10,26 @@ public class Health : MonoBehaviour
 
     private bool _alive = true;
 
+    [SerializeField]
+    private UnityEvent<int> _onTakeDamage;
+    [SerializeField]
+    private UnityEvent _onDeath;
+
     public int Lives { get => _lives; set => _lives = value; }
     public int MaxLives { get => _maxLives; set => _maxLives = value; }
     public bool Alive { get => _alive; set => _alive = value; }
 
+    public UnityEvent<int> OnTakeDamage { get => _onTakeDamage; }
+    public UnityEvent OnDeath { get => _onDeath; }
+
     public virtual void Die()
     {
+        Debug.Log("Died!");
+        
         _lives = 0;
         _alive = false;
 
-        Debug.Log("Died!");
+        _onDeath.Invoke();
     }
 
     public virtual void Damage(int lives)
@@ -27,6 +38,7 @@ public class Health : MonoBehaviour
             return;
 
         _lives = Mathf.Max(_lives - lives, 0);
+        _onTakeDamage.Invoke(lives);
         Debug.Log($"Lost {lives} lives! Remaining: {_lives}");
 
         if (_lives == 0)
