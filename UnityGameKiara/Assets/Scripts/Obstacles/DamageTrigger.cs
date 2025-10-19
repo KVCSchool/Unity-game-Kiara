4bceb2559229d9a261438f8a2cef2d3b55e47146
KVCSchool <kiano.van.cauter@ll.sintgregorius.be>
2025-10-19 20:40:24 +0200

    Obstacles: push rigid bodies back from damage triggers properly

--- UnityGameKiara/Assets/Scripts/Obstacles/DamageTrigger.cs
+++ UnityGameKiara/Assets/Scripts/Obstacles/DamageTrigger.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class DamageTrigger : MonoBehaviour
 {
+    private Collider2D _trigerCollider;
+
     [SerializeField]
     private int _damage = 1;
 
@@ -12,12 +15,30 @@ public class DamageTrigger : MonoBehaviour
 
     public float PushbackStrength { get => _pushbackStrength; set => _pushbackStrength = value; }
 
+    private void Start()
+    {
+        _trigerCollider = GetComponent<Collider2D>();
+    }
+
+    private void PushRigidBodyBack(Rigidbody2D rigidBody)
+    {
+        //rigid body could already be past the trigger
+        Vector2 rigidBodyPrevPos = rigidBody.position - rigidBody.linearVelocity * Time.deltaTime;
+        Vector2 closestTriggerPoint = _trigerCollider.ClosestPoint(rigidBodyPrevPos);
+
+        //normalized vector pointing from rigid body's previous position
+        //to closest point on trigger collider
+        Vector2 normal = (closestTriggerPoint - rigidBodyPrevPos).normalized;
+
+        rigidBody.linearVelocity = -normal * _pushbackStrength;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Health health))
             health.Damage(_damage);
 
         if (collision.gameObject.TryGetComponent(out Rigidbody2D rigidBody))
-            rigidBody.linearVelocity = -rigidBody.linearVelocity.normalized * _pushbackStrength;
+            PushRigidBodyBack(rigidBody);
     }
 }
